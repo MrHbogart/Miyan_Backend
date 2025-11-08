a693270cdc548e43b83fd857468609ec96f8bc09
Hossein <ho.saeidiwork@gmail.com>
2025-11-08 21:57:03 +0330

    added ssl redirect to the settings

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+SECURE_SSL_REDIRECT = True
+
 # Application definition
 
 INSTALLED_APPS = [
