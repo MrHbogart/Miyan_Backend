cde8e587a83eed4f475963ee1391bfc43f2017eb
Hossein <ho.saeidiwork@gmail.com>
2025-11-08 22:06:18 +0330

    some missing commas

--- miyanMadi/admin.py
+++ miyanMadi/admin.py
@@ -3,6 +3,6 @@ from .models import MiyanMadiMenu
 
 @admin.register(MiyanMadiMenu)
 class MiyanMadiMenuAdmin(admin.ModelAdmin):
-    list_display = ('name_fa', 'name_en' 'category', 'price', 'subtitle_fa', 'subtitle_en', 'available', 'created_at', 'image')
+    list_display = ('name_fa', 'name_en', 'category', 'price', 'subtitle_fa', 'subtitle_en', 'available', 'created_at', 'image')
     list_filter = ('category', 'available')
     search_fields = ('name',)
