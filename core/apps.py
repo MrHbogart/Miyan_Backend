42e5ac5139267f3dfac459e8673341172a89a1b8
Hossein <ho.saeidiwork@gmail.com>
2025-11-09 14:34:39 +0330

    commenting some inwanted lines of codes

--- core/apps.py
+++ core/apps.py
@@ -1,17 +1,17 @@
 from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+# class CoreConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'core'
 
-    def ready(self):
-        # create default groups if they don't exist yet (safe on migrations)
-        try:
-            from django.contrib.auth.models import Group
+#     def ready(self):
+#         # create default groups if they don't exist yet (safe on migrations)
+#         try:
+#             from django.contrib.auth.models import Group
 
-            Group.objects.get_or_create(name='staff')
-            Group.objects.get_or_create(name='managers')
-        except Exception:
-            # during initial migrate the auth tables may not be ready; ignore errors
-            pass
+#             Group.objects.get_or_create(name='staff')
+#             Group.objects.get_or_create(name='managers')
+#         except Exception:
+#             # during initial migrate the auth tables may not be ready; ignore errors
+#             pass
