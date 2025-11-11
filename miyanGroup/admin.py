f244a79b8065d0f67bc6141bb3dd722491aee90a
Hossein <ho.saeidiwork@gmail.com>
2025-11-11 15:00:21 +0330

    debug

--- miyanGroup/admin.py
+++ miyanGroup/admin.py
@@ -3,8 +3,8 @@ from .models import MiyanGallery
 
 @admin.register(MiyanGallery)
 class MiyanGalleryAdmin(admin.ModelAdmin):
-    list_display = ['title_en', 'title_fa', 'display_order']
-    search_fields = ['title_en', 'title_fa', 'display_order']
+    list_display = ['title_en', 'title_fa', 'order']
+    search_fields = ['title_en', 'title_fa']
 
 
 
